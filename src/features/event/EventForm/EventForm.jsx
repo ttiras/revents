4f22b89b4967b919d70bb66ad1322d49f4400dca
ttiras <tiras@europe.com>
2019-10-16 11:43:49 +0300

    en of section 4

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -2,30 +2,83 @@ import React, { Component } from 'react'
 import { Segment, Form, Button } from 'semantic-ui-react'
 
 class EventForm extends Component {
+  state = {
+    title: '',
+    date: '',
+    city: '',
+    venue: '',
+    hostedBy: ''
+  }
+
+  componentDidMount () {
+    if (this.props.selectedEvent !== null){
+      this.setState({
+        ...this.props.selectedEvent
+      })
+    }
+  }
+
+  handleFormSubmit = e => {
+    e.preventDefault()
+    if (this.state.id) {
+      this.props.updateEvent(this.state)
+    } else {
+      this.props.createEvent(this.state)
+    }
+  }
+
+  handleInputForm = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+    
+  }
+
     render() {
         const { formToggle } = this.props
+        const {title, date, city, venue, hostedBy} = this.state
         return (
                   <Segment>
-                    <Form>
+                    <Form onSubmit={this.handleFormSubmit}>
                       <Form.Field>
                         <label>Event Title</label>
-                        <input placeholder="First Name" />
+                        <input 
+                          name='title'
+                          value= {title}
+                          onChange={this.handleInputForm}
+                          placeholder="Event Title" />
                       </Form.Field>
                       <Form.Field>
                         <label>Event Date</label>
-                        <input type="date" placeholder="Event Date" />
+                        <input 
+                        name='date'
+                        value= {date}
+                        onChange={this.handleInputForm}
+                        type="date" placeholder="Event Date" />
                       </Form.Field>
                       <Form.Field>
                         <label>City</label>
-                        <input placeholder="City event is taking place" />
+                        <input 
+                        name='city'
+                        value= {city}
+                        onChange={this.handleInputForm}
+                        placeholder="City event is taking place" />
                       </Form.Field>
                       <Form.Field>
                         <label>Venue</label>
-                        <input placeholder="Enter the Venue of the event" />
+                        <input 
+                        name='venue'
+                        value= {venue}
+                        onChange={this.handleInputForm}
+                        placeholder="Enter the Venue of the event" />
                       </Form.Field>
                       <Form.Field>
                         <label>Hosted By</label>
-                        <input placeholder="Enter the name of person hosting" />
+                        <input 
+                        name='hostedBy'
+                        value= {hostedBy}
+                        onChange={this.handleInputForm}
+                        placeholder="Enter the name of person hosting" />
                       </Form.Field>
                       <Button positive type="submit">
                         Submit
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -4,7 +4,7 @@ import EventListAtandee from '../EventList/EventListAtandee'
 
 class EventListItem extends Component {
     render() {
-        const { event } = this.props
+        const { event, selectEvent, deleteEvent } = this.props
         return (
                  <Segment.Group>
                     <Segment>
@@ -14,7 +14,7 @@ class EventListItem extends Component {
                           <Item.Content>
                             <Item.Header as="a">{event.title}</Item.Header>
                             <Item.Description>
-                              Hosted by <a>{event.hostedBy}</a>
+                              Hosted by {event.hostedBy}
                             </Item.Description>
                           </Item.Content>
                         </Item>
@@ -28,14 +28,15 @@ class EventListItem extends Component {
                     </Segment>
                     <Segment secondary>
                       <List horizontal>
-                          {event.attendees.map(attendee => (
+                          {event.attendees && event.attendees.map(attendee => (
                             <EventListAtandee key={attendee.id} attendee={attendee} />
                           ))}
                       </List>
                     </Segment>
                     <Segment clearing>
                         <span>{event.description}</span>
-                      <Button as="a" color="teal" floated="right" content="View" />
+                      <Button onClick={()=>deleteEvent(event.id)} as="a" color="red" floated="right" content="Delete" />
+                      <Button onClick={()=>selectEvent(event)} as="a" color="teal" floated="right" content="View" />
                     </Segment>
                   </Segment.Group>
         )
